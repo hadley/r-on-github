ed438086dd1df7a967bff4ceb4c2e98f4e3624fc
hadley <h.wickham@gmail.com>
2013-07-25 12:56:14 -0500

    Add comments to 2-packages

--- 2-packages.r
+++ 2-packages.r
@@ -1,20 +1,26 @@
 library(ggplot2)
 library(plyr)
 library(reshape2)
+"%||%" <- function(a, b) if (length(a) == 0) b else a
 
+# Load repo data
 repos <- llply(dir("cache-repo", full.names = TRUE), readRDS)
 names(repos) <- vapply(repos, function(x) x$info$full_name, character(1))
 
+# Focus on repos with valid DESCRIPTION - i.e. packages
 has_desc <- vapply(repos, function(x) !is.null(x$desc) && is.list(x$desc), logical(1))
 pkgs <- repos[has_desc]
 
-"%||%" <- function(a, b) if (length(a) == 0) b else a
+# Look at all the titles
 lapply(pkgs, function(x) x$desc$Title %||% "")
 
+# Find which ones are on cran already
 names <- unname(vapply(pkgs, function(x) x$desc$Package %||% "", character(1)))
 available <- available.packages()
 on_cran <- names %in% rownames(available)
 sum(on_cran)
+
+# Explore dependencies ---------------------------------------------------------
 
 deps <- lapply(pkgs, function(x) {
   deps <- NULL
@@ -27,11 +33,9 @@ deps <- lapply(pkgs, function(x) {
   })
 })
 
+# Most common dependencies
 head(arrange(count(unlist(deps)), desc(freq)), 20)
 
+# Which use Rcpp
 rcpp <- vapply(deps, function(x) "Rcpp" %in% x, logical(1))
 names(pkgs)[rcpp]
-
-
-
-lapply(pkgs, function(x) parse_deps(x$Imports))
