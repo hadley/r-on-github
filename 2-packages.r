2939251592c5221c1e577ec33565c162d6c3930a
hadley <h.wickham@gmail.com>
2013-10-30 11:47:51 -0500

    Code to plot rstats repos over time

--- 2-packages.r
+++ 2-packages.r
@@ -1,14 +1,19 @@
 library(ggplot2)
 library(plyr)
 library(reshape2)
+library(lubridate)
 "%||%" <- function(a, b) if (length(a) == 0) b else a
 
 # Load repo data
 repos <- llply(dir("cache-repo", full.names = TRUE), readRDS)
 names(repos) <- vapply(repos, function(x) x$info$full_name, character(1))
 
+created <- vapply(repos, function(x) x$info$created_at, character(1), USE.NAMES = FALSE)
+created <- as.Date(created)
+
 # Focus on repos with valid DESCRIPTION - i.e. packages
 has_desc <- vapply(repos, function(x) !is.null(x$desc) && is.list(x$desc), logical(1))
+
 pkgs <- repos[has_desc]
 
 # Look at all the titles
@@ -19,6 +24,22 @@ names <- unname(vapply(pkgs, function(x) x$desc$Package %||% "", character(1)))
 available <- available.packages()
 on_cran <- names %in% rownames(available)
 sum(on_cran)
+
+# Explore over time ---------------------------------------------------------
+
+repo_df <- data.frame(
+  name = names(repos), 
+  created = created, 
+  type = ifelse(has_desc, "pkg", "repo")
+)
+counts <- count(repo_df, c("type", "created"))
+counts <- ddply(counts, "type", mutate, csum = cumsum(freq))
+qplot(created, csum, data = counts, geom = "line", colour = type) + 
+  labs(x = NULL, y = "Cumulative count")
+ggsave("github-repos.pdf", width = 8, height = 6)
+last_plot() + scale_y_log10()
+ggsave("github-repos-log.pdf", width = 8, height = 6)
+
 
 # Explore dependencies ---------------------------------------------------------
 
